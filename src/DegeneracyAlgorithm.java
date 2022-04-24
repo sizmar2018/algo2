457fc9bdbbc39c5566f47bfbd00ad0be4457aca0
mustivore <mustapha-ayadi@hotmail.be>
2022-04-24 09:52:07 +0200

    nouvel algo

--- src/DegeneracyAlgorithm.java
+++ src/DegeneracyAlgorithm.java
@@ -4,61 +4,48 @@ import java.util.*;
 
 public class DegeneracyAlgorithm {
 
-    private final Graph g;
+    private final Graph G;
 
     public DegeneracyAlgorithm(Graph graph) {
-        this.g = graph;
+        this.G = graph;
     }
 
     public int getKDegenerate(){
-        int[] vDegree = new int[g.V()];
-        Set<Integer> vDeleted = new HashSet<>();
-        int maxDegree = Integer.MIN_VALUE;
-        for (int i = 0; i < g.V(); i++){
-            int degree = g.degree(i);
-            vDegree[i] = degree;
-            if (degree > maxDegree){
-                maxDegree = degree;
+        int[] vDegree = new int[G.V()];
+        boolean[] vDeleted = new boolean[G.V()];
+        int sigma = (int) Double.POSITIVE_INFINITY;
+
+        for (int i = 0; i < G.V(); i++){
+            vDegree[i] = G.degree(i);
+            if(G.degree(i) < sigma){
+                sigma = G.degree(i);
             }
         }
-        int[] vCores = new int[g.V()];
-        HashSet<Integer>[] vectorsByDegree = new HashSet[maxDegree + 1];
-        for(int i = 0; i < maxDegree + 1; i++){
-            vectorsByDegree[i] = new HashSet<>();
-        }
-        for(int i = 0; i < g.V(); i++){
-            vectorsByDegree[g.degree(i)].add(i);
-        }
-
-        int k = 0;
-        for(int i = 0; i < g.V(); i++){
-            int j = getMinDegree(vectorsByDegree);
-            k = Math.max(j, k);
-            int v = getVertexFromSet(vectorsByDegree[j]);
-            vectorsByDegree[j].remove(v);
-            vDeleted.add(v);
-            vCores[v] = k;
-            vDegree[v] = 0;
-            for(int w: g.adj(v)){
-                if(!vDeleted.contains(w)){
-                    int wDegree = vDegree[w];
-                    vectorsByDegree[wDegree].remove(w);
-                    vectorsByDegree[wDegree - 1].add(w);
-                    vDegree[w] = wDegree - 1;
+        int nbVertexDeleted = 0;
+        int k = sigma;
+        while (nbVertexDeleted != G.V()){
+            boolean noVertexDeleted = true;
+            for (int i = 0; i < G.V(); i++){
+                if(!vDeleted[i] && vDegree[i] <= k){
+                    vDeleted[i] = true;
+                    vDegree[i] = 0;
+                    noVertexDeleted = false;
+                    nbVertexDeleted += 1;
+                    for(int voisin: G.adj(i)){
+                        if(!vDeleted[voisin]){
+                            vDegree[voisin] -= 1;
+                        }
+                    }
                 }
+            }
+            if (noVertexDeleted){
+                k+=1;
             }
         }
         return k;
     }
 
-    private int getVertexFromSet(HashSet<Integer> vertexes) {
-        for (int v: vertexes){
-            return v;
-        }
-        throw new RuntimeException("Empty set");
-    }
-
-    private int getMinDegree(HashSet<Integer>[] vectorsByDegree) {
+    private int getMinDegree(ArrayList<Integer>[] vectorsByDegree) {
         int j = 0;
         while (j < vectorsByDegree.length){
             if(!vectorsByDegree[j].isEmpty()){
